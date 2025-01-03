4a1ef62403866d18bb6d5f0e170f57233f9c8f4d
reshreshu <reshreshualavutheen09@gmail.com>
2025-01-03 10:56:00 +0530

    added Day_3

---
+++ Day_3/task1.js
@@ -0,0 +1,27 @@
+// Array iteration using forEach
+let array = [1, 2, 3, 4, 5];
+array.forEach((element, index) => {
+    console.log(`Element is ${element} and its index is ${index}`);
+});
+
+// Object iteration using for...in and proper key-value access
+let objtype = {
+    "firstname": "Reshma",
+    "lastname": "Alavutheen",
+};
+
+/* Commented out incorrect examples
+console.log(objtype.age);
+objtype.forEach((key, value) => { // forEach doesn't work on objects
+    console.log(value, key);
+});
+for (const {key, value} of objtype) { // Incorrect syntax for objects
+    console.log(key, value);
+}
+*/
+
+// Corrected object iteration using for...in
+for (let key in objtype) {
+    let value = objtype[key]; // Proper way to access object values
+    console.log(`${key} : ${value}`);
+}
